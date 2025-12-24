82ca9d3d1b2e89d4ea8640f3e99a7e6688947bbd
Almos-Pal <75310455+Almos-Pal@users.noreply.github.com>
2025-12-24 15:12:06 +0100

    chane test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,23 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { monthlyDates } from "@/data/monthlyDates";
 import { MonthlyDate } from "@/types/monthlyDate";
 import QRCode from "react-qr-code";
 export default function page() {
   const [monthData, setMonthData] = useState<MonthlyDate | null>(null);
+  const [baseUrl, setBaseUrl] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setBaseUrl(window.location.origin);
+    }
+  }, []);
   return (
     <div className="flex items-start gap-4 max-w-[500px] flex-wrap">
       {monthlyDates.map((month) => {
         return (
           <div key={month.month}>
-            <QRCode value={month.month.toString()} size={150} />
+            <QRCode value={`${baseUrl}/month/${month.month}`} size={150} />
           </div>
         );
       })}
